b88a1b978455006161b52a0541b4cbed85f62664
Tristan <tristantcate@gmail.com>
2020-10-17 08:33:52 +0200

    basic randomised trees added, decoration system added using scriptableObjs

---
+++ Assets/DecorationSpecifier.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DecorationSpecifier", menuName = "Parallax", order = 1)]
+public class DecorationSpecifier : ScriptableObject
+{
+    public Sprite[] m_randomSprites;
+
+    [Space(10)]
+    public float m_scaleAdjust;
+    public float m_thickness;
+
+    [Space(10)]
+    public Vector2 m_minDistanceInBetween;
+    public Vector2 m_maxDistanceInBetween;
+
+    [Space(10)]
+    public Vector2 m_decoAreaMin;
+    public Vector2 m_decoAreaMax;
+
+}
--- Assets/ParallaxManager.cs
+++ Assets/ParallaxManager.cs
@@ -22,9 +22,16 @@ public class ParallaxManager : MonoBehaviour
         public float m_spriteScaleAdjustment;
         public bool m_flipXPerSprite;
 
+        [Header("Random Decoration")]
+        public DecorationSpecifier[] m_decorations;
+
         [HideInInspector] public Transform m_parallaxLayerObject;
         [HideInInspector] public Transform[] m_parallaxObjects;
+
+        [HideInInspector] public List<GameObject>[] m_decoPrefabInstances;
     }
+
+   
 
     [SerializeField] private ParallaxLayer[] m_parallaxLayers;
 
@@ -52,11 +59,16 @@ public class ParallaxManager : MonoBehaviour
 
                 SpriteRenderer sr = parallaxObj.AddComponent<SpriteRenderer>();
                 sr.sprite = parallaxLayer.m_sprite;
-                sr.sortingOrder = parallaxLayer.m_spriteRenderLayer;
+                sr.sortingOrder = parallaxLayer.m_spriteRenderLayer * 1000;
                 sr.sortingLayerName = "Parallax";
 
                 if (parallaxLayer.m_flipXPerSprite && parallaxObjIter % 2 == 1) sr.flipX = true;
             }
+
+            //Setup list of decorations
+            parallaxLayer.m_decoPrefabInstances = new List<GameObject>[parallaxObjAmount];
+            for (int i = 0; i < parallaxLayer.m_decoPrefabInstances.Length; i++)
+                parallaxLayer.m_decoPrefabInstances[i] = new List<GameObject>();
         }
     }
 
@@ -90,10 +102,65 @@ public class ParallaxManager : MonoBehaviour
 
                     if(parallaxLayer.m_flipXPerSprite && parallaxLayer.m_parallaxObjects.Length % 2 == 1)
                     {
-                        parallaxObj.GetComponent<SpriteRenderer>().flipX = !parallaxObj.GetComponent<SpriteRenderer>().flipX;
+                        SpriteRenderer parallaxObjSR = parallaxObj.GetComponent<SpriteRenderer>();
+                        parallaxObjSR.flipX = !parallaxObjSR.flipX;
                     }
+
+                    AddDecorations(parallaxObj, parallaxLayer);
                 }
             }
         }
+    }
+
+    private void AddDecorations(Transform a_parent, ParallaxLayer a_parallaxLayer)
+    {
+        foreach (GameObject currentDecoInstances in a_parallaxLayer.m_decoPrefabInstances[GetParallaxObjectID(a_parent, a_parallaxLayer)])
+        {
+            Destroy(currentDecoInstances);
+        }
+
+        a_parallaxLayer.m_decoPrefabInstances[GetParallaxObjectID(a_parent, a_parallaxLayer)].Clear();
+
+        DecorationSpecifier currentParallaxDeco = a_parallaxLayer.m_decorations[0];
+        for (
+            float 
+            y = currentParallaxDeco.m_decoAreaMin.y;
+            y < currentParallaxDeco.m_decoAreaMax.y;
+            y += Random.Range(currentParallaxDeco.m_minDistanceInBetween.y, currentParallaxDeco.m_maxDistanceInBetween.y)
+            ) {
+
+            for (
+                float
+                x = currentParallaxDeco.m_decoAreaMin.x;
+                x < currentParallaxDeco.m_decoAreaMax.x;
+                x += Random.Range(currentParallaxDeco.m_minDistanceInBetween.x, currentParallaxDeco.m_maxDistanceInBetween.x)
+                )
+            {
+                GameObject decoObj = new GameObject(string.Format("decoObj[{0},{1}]", x, y));
+                decoObj.transform.parent = a_parent;
+                //decoObj.transform.position = Vector3.zero;
+                decoObj.transform.localPosition = new Vector3(x, y);
+                decoObj.transform.localScale = Vector3.one * currentParallaxDeco.m_scaleAdjust;
+                a_parallaxLayer.m_decoPrefabInstances[GetParallaxObjectID(a_parent, a_parallaxLayer)].Add(decoObj);
+
+                SpriteRenderer sr = decoObj.AddComponent<SpriteRenderer>();
+                sr.sprite = currentParallaxDeco.m_randomSprites[Random.Range(0, currentParallaxDeco.m_randomSprites.Length)];
+                sr.sortingLayerName = "Parallax";
+                sr.sortingOrder = a_parallaxLayer.m_spriteRenderLayer * 1000;
+                sr.sortingOrder += Mathf.RoundToInt(-y * 10.0f) + 500;
+            }
+        }
+
+    }
+
+    private int GetParallaxObjectID(Transform a_parallaxObj, ParallaxLayer a_layer)
+    {
+        for(int i = 0; i < a_layer.m_parallaxObjects.Length; i++)
+        {
+            if (a_layer.m_parallaxObjects[i] == a_parallaxObj) return i;
+        }
+        return -1;
+
+        Debug.LogError("NO PARALLAXOBJECT ID FOUND!");
     }
 }
